14fe8631baf3aaf10f3516ef27828b40bd038b45
Zuxriddin <56336321+zuxriddin99@users.noreply.github.com>
2021-03-06 11:37:51 +0500

    delete migrations file

--- construction/telegram_bot.py
+++ construction/telegram_bot.py
@@ -4,8 +4,10 @@ from mainapp.models import Korxona
 BOT_TOKEN = '1652636238:AAEaM3xVl2WdZnb7oWMT9YmEg618MiZf4JU'
 BOT_URL = 'https://api.telegram.org/bot%s' % BOT_TOKEN
 BOT_CHAT_ID = '630613565'
-
-telegram_id = str(Korxona.objects.get(id=1).telegram)
+try:
+    telegram_id = str(Korxona.objects.get(id=1).telegram)
+except:
+    pass
 
 
 def send_message_via_bot(request, msg):
@@ -14,4 +16,3 @@ def send_message_via_bot(request, msg):
         bot.sendMessage(telegram_id, text=msg, parse_mode='html')
     except:
         pass
-
