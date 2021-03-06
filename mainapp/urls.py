7041d1c23079457022f82a000179ed348f07c9ab
Zuxriddin <56336321+zuxriddin99@users.noreply.github.com>
2021-03-07 02:29:28 +0500

    fixed index page

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from mainapp.views import BoshSaxifaView, YangilikView, KorxonaView, XabarView
+from mainapp.views import BoshSaxifaView, YangilikView, KorxonaView, XabarView, BizbingIshView
 
 urlpatterns = [
     path('', BoshSaxifaView.as_view(), name='yangiliklar'),
     path('yangilik/<int:pk>/', YangilikView.as_view(), name='yangilik'),
     path('korxona/<int:pk>/', KorxonaView.as_view(), name='korxona'),
-    path('xabar/', XabarView.as_view(), name='xabar')
+    path('xabar/', XabarView.as_view(), name='xabar'),
+    path('ish/<int:pk>/', BizbingIshView.as_view(), name='bizni-korxona'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,12 @@
+from django.contrib import messages
 from django.shortcuts import render
 from .models import Yangilik, Xodim, BizningIshlarimiz, Korxona, Xabar
 from construction.telegram_bot import send_message_via_bot
 # Create your views here.
 from django.views.generic import ListView, DetailView, CreateView
+from datetime import date
+
+today = date.today()
 
 
 class BoshSaxifaView(ListView):
@@ -28,6 +32,16 @@ class YangilikView(DetailView):
         return context
 
 
+class BizbingIshView(DetailView):
+    queryset = BizningIshlarimiz.objects.all()
+    template_name = 'work.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['yangiliklar'] = Yangilik.objects.all().exclude(id=self.kwargs['pk']).order_by('-created_dt')
+        return context
+
+
 class KorxonaView(DetailView):
     queryset = Korxona.objects.all()
     template_name = 'about_page.html'
@@ -42,7 +56,7 @@ class XabarView(CreateView):
     model = Xabar
     template_name = 'index.html'
     fields = ['name', 'phone', 'message']
-    success_url = '/'
+    success_url = '/#id7'
 
     def post(self, request, *args, **kwargs):
         super().post(request, *args, **kwargs)
@@ -50,6 +64,11 @@ class XabarView(CreateView):
         message = '<i>Sizga yangi mijoz xabar qoldirdi.</i>\n'
         message += 'Mijozning ismi:  <b>' + data['name'] + '</b>'
         message += '\nMijozning raqami:   <b>' + data['phone'] + '</b>'
-        message += '\nXabar: \n <b>' + data['message'] + '</b>'
-        send_message_via_bot(request, message)
+        message += '\nXabar: \n <b>' + data['message'] + '</b>\n\n'
+        message += today.strftime("%d/%m/%Y")
+        try:
+            send_message_via_bot(request, message)
+            messages.info(request, 'Sizning a\'rizangiz korxona raxbariga yetkazildi. Tez orada sizga bog\'lanishadi')
+        except:
+            pass
         return super().post(request, *args, **kwargs)
